708dacce2f3c3059011a4b29631a093c95c87b9b
dwane richards <dwanerich@gmail.com>
2020-03-02 22:43:45 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,8 +51,9 @@ class InteractiveRecord
     #{table_name_for_insert}")[0][0]
   end
   
-  def self.find_by_name
-    
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
   end
 
   
